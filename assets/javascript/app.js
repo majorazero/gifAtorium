c66bc83385a8f50bd3b8360168c1a37cfd8daef0
majorazero <daniel.cw.hsu@gmail.com>
2018-08-31 12:28:15 -0700

    replaced onclick functionaity to hover

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ let apikey = "VHi8AqnqicsB04oW6P1Vj8bjeIGMXBrc";
 let topics = ["reactions", "scoot", "spooky",
               "sloth","cats","nope","nigel"];
 let offset = 0; // this is going to stop repeating images in get more gif's
+let hoverCount = 0;
 pageGenerator();
 //we'll write the topic button generator
 function pageGenerator(){
@@ -35,6 +36,7 @@ function gifMaker(response){
   //will create 10 of these
   for(let i = 0; i < 10; i++){
     let pictureFrame = $("<div>");
+    pictureFrame.addClass("pictureFrame");
     //let's set the rating of the pic here.
     pictureFrame.append("<h2 class='gifRating'> Rating: "+response.data[i].rating+" </h2");
     //we'll set a custom attribute
@@ -45,9 +47,10 @@ function gifMaker(response){
     //sticks default still image on
     $(pictureFrame).append("<img class='img-fluid' src='"+pictureFrame.attr("stillSrc")+"' />");
     //add the click image functionality
-    pictureFrame.on("click",function(){
+    pictureFrame.on("mouseenter",function(){
       //if attribute is moving
       if($(this).attr("isMoving") === "false"){
+        hoverCount++;
         $(this).attr("isMoving","true");
         //this series of wierd code is written to prevent a clipping issue when loading images.
         let movingImg = $("<img>").attr("src",$(this).attr("animateSrc"));
@@ -58,11 +61,10 @@ function gifMaker(response){
           $(thisButton).append(this);
         });
       }
-      else{
-        $(this).attr("isMoving","false");
-        $(this).children().last().remove();
-        $(this).append("<img class='img-fluid' src='"+pictureFrame.attr("stillSrc")+"' />");
-      }
+    }).on("mouseleave",function(){
+      $(this).attr("isMoving","false");
+      $(this).children().last().remove();
+      $(this).append("<img class='img-fluid' src='"+pictureFrame.attr("stillSrc")+"' />");
     });
     $("#gifGallery").prepend(pictureFrame);
   }
@@ -70,12 +72,14 @@ function gifMaker(response){
 //new topic generator
 $("#newTopicForm").submit(function(){
   event.preventDefault();
-  //pushes latest input into the array
-  topics.push($("#newTopics").val());
-  //clears it
-  $("#newTopics").html("");
-  //re-runs page generator
-  pageGenerator();
+  if(topics.indexOf($("#newTopics").val()) === -1){
+    //pushes latest input into the array
+    topics.push($("#newTopics").val());
+    //clears it
+    $("#newTopics").html("");
+    //re-runs page generator
+    pageGenerator();
+  }
 });
 //the request additioanl gif function
 $("#requestMore").on("click",function(){
